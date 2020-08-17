a9bbc48196ab881e266b73de61eb2ecacc1d886e
jmbhughes <hughes.jmb@gmail.com>
2020-08-17 04:08:06 -0600

    version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='solarannotator',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.2.5',
+    version='0.2.6',
     packages=['solarannotator'],
     url='',
     license='',
