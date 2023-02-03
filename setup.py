1dfbab40f87dd177d22789e1dfa7bcb5e16fb251
jmbhughes <hughes.jmb@gmail.com>
2023-02-03 07:59:58 -0700

    increments version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='solarannotator',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.2.15',
+    version='0.3.0',
     packages=['solarannotator'],
     url='',
     license='',
