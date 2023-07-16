d0b0dae3d61ecd6dec17f9341a5e2f06fa9922f3
Marcus Hughes <hughes.jmb@gmail.com>
2023-07-16 16:40:47 -0600

    Increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='solarannotator',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.0.0',
+    version='1.0.1',
     packages=['solarannotator'],
     url='',
     license='',
